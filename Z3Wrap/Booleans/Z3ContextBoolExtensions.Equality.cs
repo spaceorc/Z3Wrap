1da8e6884d3f8822e45e388341272accda6faaa3
spaceorc <spaceorc@yandex.ru>
2025-09-26 22:47:29 +0100

    feat: introduce Z3Bool class with logical operations and implicit conversions

---
+++ Z3Wrap/Booleans/Z3ContextBoolExtensions.Equality.cs
@@ -0,0 +1,37 @@
+using Spaceorc.Z3Wrap.Expressions;
+using Spaceorc.Z3Wrap.Interop;
+
+namespace Spaceorc.Z3Wrap.Booleans;
+
+/// <summary>
+/// Provides extension methods for Z3Context to work with boolean equality operations.
+/// </summary>
+public static partial class Z3ContextBoolExtensions
+{
+    /// <summary>
+    /// Creates a Boolean expression representing equality between two Boolean expressions.
+    /// </summary>
+    /// <param name="context">The Z3 context.</param>
+    /// <param name="left">The left Boolean expression.</param>
+    /// <param name="right">The right Boolean expression.</param>
+    /// <returns>A Boolean expression representing left == right.</returns>
+    public static Z3Bool Eq(this Z3Context context, Z3Bool left, Z3Bool right)
+    {
+        var handle = SafeNativeMethods.Z3MkEq(context.Handle, left.Handle, right.Handle);
+        return Z3Expr.Create<Z3Bool>(context, handle);
+    }
+
+    /// <summary>
+    /// Creates a Boolean expression representing inequality between two Boolean expressions.
+    /// </summary>
+    /// <param name="context">The Z3 context.</param>
+    /// <param name="left">The left Boolean expression.</param>
+    /// <param name="right">The right Boolean expression.</param>
+    /// <returns>A Boolean expression representing left != right.</returns>
+    public static Z3Bool Neq(this Z3Context context, Z3Bool left, Z3Bool right)
+    {
+        var eqHandle = SafeNativeMethods.Z3MkEq(context.Handle, left.Handle, right.Handle);
+        var handle = SafeNativeMethods.Z3MkNot(context.Handle, eqHandle);
+        return Z3Expr.Create<Z3Bool>(context, handle);
+    }
+}
---
+++ Z3Wrap/Booleans/Z3ContextBoolExtensions.cs
@@ -0,0 +1,62 @@
+using Spaceorc.Z3Wrap.Expressions;
+using Spaceorc.Z3Wrap.Interop;
+
+namespace Spaceorc.Z3Wrap.Booleans;
+
+/// <summary>
+/// Provides extension methods for Z3Context to work with boolean expressions, constants, and literals.
+/// Supports boolean logic operations including true/false constants and boolean variables.
+/// </summary>
+public static partial class Z3ContextBoolExtensions
+{
+    /// <summary>
+    /// Creates a boolean expression from the specified boolean value.
+    /// </summary>
+    /// <param name="context">The Z3 context.</param>
+    /// <param name="value">The boolean value to convert.</param>
+    /// <returns>A new Z3Bool representing the boolean value.</returns>
+    public static Z3Bool Bool(this Z3Context context, bool value)
+    {
+        var handle = value
+            ? SafeNativeMethods.Z3MkTrue(context.Handle)
+            : SafeNativeMethods.Z3MkFalse(context.Handle);
+        return Z3Expr.Create<Z3Bool>(context, handle);
+    }
+
+    /// <summary>
+    /// Creates a boolean expression representing the value true.
+    /// </summary>
+    /// <param name="context">The Z3 context.</param>
+    /// <returns>A new Z3Bool representing true.</returns>
+    public static Z3Bool True(this Z3Context context)
+    {
+        var handle = SafeNativeMethods.Z3MkTrue(context.Handle);
+        return Z3Expr.Create<Z3Bool>(context, handle);
+    }
+
+    /// <summary>
+    /// Creates a boolean expression representing the value false.
+    /// </summary>
+    /// <param name="context">The Z3 context.</param>
+    /// <returns>A new Z3Bool representing false.</returns>
+    public static Z3Bool False(this Z3Context context)
+    {
+        var handle = SafeNativeMethods.Z3MkFalse(context.Handle);
+        return Z3Expr.Create<Z3Bool>(context, handle);
+    }
+
+    /// <summary>
+    /// Creates a boolean constant (variable) with the specified name.
+    /// </summary>
+    /// <param name="context">The Z3 context.</param>
+    /// <param name="name">The name of the boolean constant.</param>
+    /// <returns>A new Z3Bool representing the boolean constant.</returns>
+    public static Z3Bool BoolConst(this Z3Context context, string name)
+    {
+        using var namePtr = new AnsiStringPtr(name);
+        var symbol = SafeNativeMethods.Z3MkStringSymbol(context.Handle, namePtr);
+        var boolSort = SafeNativeMethods.Z3MkBoolSort(context.Handle);
+        var handle = SafeNativeMethods.Z3MkConst(context.Handle, symbol, boolSort);
+        return Z3Expr.Create<Z3Bool>(context, handle);
+    }
+}
