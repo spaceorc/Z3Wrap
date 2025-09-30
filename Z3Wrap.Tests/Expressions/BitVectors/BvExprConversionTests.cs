c5e92166c7e096850b9f56a4b826b18815e7ebd8
spaceorc <spaceorc@yandex.ru>
2025-10-01 00:01:52 +0100

    feat: add tests for bit-vector expression conversion and factory methods

---
+++ Z3Wrap.Tests/Expressions/BitVectors/BvExprConversionTests.cs
@@ -0,0 +1,290 @@
+using System.Numerics;
+using Spaceorc.Z3Wrap.Core;
+using Spaceorc.Z3Wrap.Expressions.BitVectors;
+using Spaceorc.Z3Wrap.Values.BitVectors;
+
+namespace Z3Wrap.Tests.Expressions.BitVectors;
+
+/// <summary>
+///     Tests for bit-vector expression conversion methods.
+/// </summary>
+[TestFixture]
+public class BvExprConversionTests
+{
+    [TestCase(TypeArgs = [typeof(Size32)])]
+    [TestCase(TypeArgs = [typeof(Size64)])]
+    public void ToInt_UnsignedConversion_ConvertsCorrectly<TSize>()
+        where TSize : ISize
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvValue = context.BitVec<TSize>(42u);
+        var intValue = bvValue.ToInt();
+        var intValueViaContext = context.ToInt(bvValue);
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model.GetIntValue(intValue), Is.EqualTo(new BigInteger(42)));
+            Assert.That(model.GetIntValue(intValueViaContext), Is.EqualTo(new BigInteger(42)));
+        });
+    }
+
+    [Test]
+    public void ToInt_SignedConversion_ConvertsNegativeValueCorrectly()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        // -42 in 32-bit two's complement
+        var bvValue = context.BitVec<Size32>(unchecked((uint)-42));
+        var intValue = bvValue.ToInt(true);
+        var intValueViaContext = context.ToInt(bvValue, true);
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model.GetIntValue(intValue), Is.EqualTo(new BigInteger(-42)));
+            Assert.That(model.GetIntValue(intValueViaContext), Is.EqualTo(new BigInteger(-42)));
+        });
+    }
+
+    [Test]
+    public void ToInt_UnsignedConversion_ConvertsLargeValueCorrectly()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        // This would be negative if interpreted as signed
+        var bvValue = context.BitVec<Size32>(0xFFFFFF00u);
+        var intValue = bvValue.ToInt(false);
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        Assert.That(model.GetIntValue(intValue), Is.EqualTo(new BigInteger(0xFFFFFF00u)));
+    }
+
+    [Test]
+    public void ToInt_WithSymbolicVariable_PreservesConstraints()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var x = context.BitVecConst<Size32>("x");
+        solver.Assert(x == 42u);
+
+        var intX = x.ToInt();
+        solver.Assert(intX > 40);
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model.GetBitVec(x).Value, Is.EqualTo(new BigInteger(42)));
+            Assert.That(model.GetIntValue(intX), Is.EqualTo(new BigInteger(42)));
+        });
+    }
+
+    [TestCase(TypeArgs = [typeof(Size8), typeof(Size16)])]
+    [TestCase(TypeArgs = [typeof(Size16), typeof(Size32)])]
+    [TestCase(TypeArgs = [typeof(Size32), typeof(Size64)])]
+    public void Resize_ZeroExtend_WorksCorrectly<TInputSize, TOutputSize>()
+        where TInputSize : ISize
+        where TOutputSize : ISize
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvValue = context.BitVec<TInputSize>(42u);
+        var resizedValue = bvValue.Resize<TOutputSize>(false);
+        var resizedValueViaContext = context.Resize<TInputSize, TOutputSize>(bvValue, false);
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model.GetBitVec(resizedValue).Value, Is.EqualTo(new BigInteger(42)));
+            Assert.That(model.GetBitVec(resizedValueViaContext).Value, Is.EqualTo(new BigInteger(42)));
+        });
+    }
+
+    [Test]
+    public void Resize_SignExtend_PreservesNegativeValue()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        // -42 in 8-bit two's complement (0xD6 = 214)
+        var bvValue = context.BitVec<Size8>((uint)unchecked((byte)-42));
+        var resizedValue = bvValue.Resize<Size32>(true);
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        // Should sign-extend to 32-bit: -42
+        Assert.That(model.GetBitVec(resizedValue).Value, Is.EqualTo(new BigInteger(unchecked((uint)-42))));
+    }
+
+    [Test]
+    public void Resize_ZeroExtend_DoesNotPreserveNegativeValue()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        // -42 in 8-bit two's complement (0xD6 = 214)
+        var bvValue = context.BitVec<Size8>((uint)unchecked((byte)-42));
+        var resizedValue = bvValue.Resize<Size32>(false);
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        // Should zero-extend to 32-bit: 214
+        Assert.That(model.GetBitVec(resizedValue).Value, Is.EqualTo(new BigInteger(214)));
+    }
+
+    [TestCase(TypeArgs = [typeof(Size32), typeof(Size16)])]
+    [TestCase(TypeArgs = [typeof(Size64), typeof(Size32)])]
+    public void Resize_Truncate_WorksCorrectly<TInputSize, TOutputSize>()
+        where TInputSize : ISize
+        where TOutputSize : ISize
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvValue = context.BitVec<TInputSize>(0x12345678u);
+        var truncatedValue = bvValue.Resize<TOutputSize>();
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        // Should truncate to lower bits
+        var outputBits = TOutputSize.Size;
+        var mask = (BigInteger.One << (int)outputBits) - 1;
+        var expected = new BigInteger(0x12345678u) & mask;
+        Assert.That(model.GetBitVec(truncatedValue).Value, Is.EqualTo(expected));
+    }
+
+    [TestCase(0u)]
+    [TestCase(4u)]
+    public void Extract_ExtractsCorrectBits(uint startBit)
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvValue = context.BitVec<Size16>(0xABCDu); // 1010101111001101
+        var extractedValue = bvValue.Extract<Size8>(startBit);
+        var extractedValueViaContext = context.Extract<Size16, Size8>(bvValue, startBit);
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        // Extract bits [startBit+7:startBit]
+        var expectedValue = (0xABCDu >> (int)startBit) & 0xFFu;
+        Assert.Multiple(() =>
+        {
+            Assert.That(model.GetBitVec(extractedValue).Value, Is.EqualTo(new BigInteger(expectedValue)));
+            Assert.That(model.GetBitVec(extractedValueViaContext).Value, Is.EqualTo(new BigInteger(expectedValue)));
+        });
+    }
+
+    [Test]
+    public void Repeat_RepeatsPatternCorrectly()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvValue = context.BitVec<Size8>(0xABu); // 10101011
+        var repeatedValue = bvValue.Repeat<Size16>();
+        var repeatedValueViaContext = context.Repeat<Size8, Size16>(bvValue);
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        // Should be 0xABAB
+        Assert.Multiple(() =>
+        {
+            Assert.That(model.GetBitVec(repeatedValue).Value, Is.EqualTo(new BigInteger(0xABABu)));
+            Assert.That(model.GetBitVec(repeatedValueViaContext).Value, Is.EqualTo(new BigInteger(0xABABu)));
+        });
+    }
+
+    [Test]
+    public void Repeat_MultipleRepetitions_WorksCorrectly()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvValue = context.BitVec<Size8>(0x12u);
+        var repeatedValue = bvValue.Repeat<Size32>();
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        // Should be 0x12121212
+        Assert.That(model.GetBitVec(repeatedValue).Value, Is.EqualTo(new BigInteger(0x12121212u)));
+    }
+
+    [Test]
+    public void ConversionChain_BitVecToIntToBitVec_WorksCorrectly()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var originalBv = context.BitVec<Size32>(42u);
+        var intValue = originalBv.ToInt();
+        var convertedBackBv = intValue.ToBitVec<Size32>();
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model.GetBitVec(originalBv).Value, Is.EqualTo(new BigInteger(42)));
+            Assert.That(model.GetIntValue(intValue), Is.EqualTo(new BigInteger(42)));
+            Assert.That(model.GetBitVec(convertedBackBv).Value, Is.EqualTo(new BigInteger(42)));
+        });
+    }
+
+    [Test]
+    public void Resize_WithSymbolicVariable_PreservesConstraints()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var x = context.BitVecConst<Size8>("x");
+        solver.Assert(x < 100u);
+
+        var resizedX = x.Resize<Size32>();
+        solver.Assert(resizedX == 42u);
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model.GetBitVec(x).Value, Is.EqualTo(new BigInteger(42)));
+            Assert.That(model.GetBitVec(resizedX).Value, Is.EqualTo(new BigInteger(42)));
+        });
+    }
+}
---
+++ Z3Wrap.Tests/Expressions/BitVectors/BvExprFactoryTests.cs
@@ -0,0 +1,274 @@
+using System.Numerics;
+using Spaceorc.Z3Wrap.Core;
+using Spaceorc.Z3Wrap.Expressions.BitVectors;
+using Spaceorc.Z3Wrap.Values.BitVectors;
+
+namespace Z3Wrap.Tests.Expressions.BitVectors;
+
+/// <summary>
+/// Tests for bit-vector expression creation methods.
+/// </summary>
+[TestFixture]
+public class BvExprFactoryTests
+{
+    [TestCase(42u)]
+    [TestCase(0u)]
+    [TestCase(255u)]
+    public void CreateBitVec_FromUint_ReturnsCorrectExpression(uint value)
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvExpr = context.BitVec<Size32>(value);
+
+        Assert.That(solver.Check(), Is.EqualTo(Z3Status.Satisfiable));
+        var model = solver.GetModel();
+        Assert.That(model.GetBitVec(bvExpr).Value, Is.EqualTo(new BigInteger(value)));
+    }
+
+    [TestCase(42)]
+    [TestCase(0)]
+    [TestCase(-1)]
+    public void CreateBitVec_FromInt_ReturnsCorrectExpression(int value)
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvExpr = context.BitVec<Size32>(value);
+
+        Assert.That(solver.Check(), Is.EqualTo(Z3Status.Satisfiable));
+        var model = solver.GetModel();
+        Assert.That(model.GetBitVec(bvExpr).Value, Is.EqualTo(new BigInteger(unchecked((uint)value))));
+    }
+
+    [Test]
+    public void CreateBitVec_FromUlong_ReturnsCorrectExpression()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvExpr = context.BitVec<Size64>(9876543210UL);
+
+        Assert.That(solver.Check(), Is.EqualTo(Z3Status.Satisfiable));
+        var model = solver.GetModel();
+        Assert.That(model.GetBitVec(bvExpr).Value, Is.EqualTo(new BigInteger(9876543210UL)));
+    }
+
+    [Test]
+    public void CreateBitVec_FromLong_ReturnsCorrectExpression()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvExpr = context.BitVec<Size64>(-42L);
+
+        Assert.That(solver.Check(), Is.EqualTo(Z3Status.Satisfiable));
+        var model = solver.GetModel();
+        Assert.That(model.GetBitVec(bvExpr).Value, Is.EqualTo(new BigInteger(unchecked((ulong)-42L))));
+    }
+
+    [Test]
+    public void CreateBitVec_FromBigInteger_ReturnsCorrectExpression()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bigValue = BigInteger.Parse("12345678901234567890");
+        var bvExpr = context.BitVec<Size64>(bigValue);
+
+        Assert.That(solver.Check(), Is.EqualTo(Z3Status.Satisfiable));
+        var model = solver.GetModel();
+        // BigInteger will be masked to 64 bits
+        var expected = bigValue & ((BigInteger.One << 64) - 1);
+        Assert.That(model.GetBitVec(bvExpr).Value, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void CreateBitVec_FromBvValue_ReturnsCorrectExpression()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvValue = new Bv<Size32>(42u);
+        var bvExpr = context.BitVec(bvValue);
+
+        Assert.That(solver.Check(), Is.EqualTo(Z3Status.Satisfiable));
+        var model = solver.GetModel();
+        Assert.That(model.GetBitVec(bvExpr).Value, Is.EqualTo(new BigInteger(42)));
+    }
+
+    [Test]
+    public void CreateBitVecConst_WithVariableName_ReturnsCorrectExpression()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvConst = context.BitVecConst<Size32>("variableName");
+
+        solver.Assert(bvConst == 42u);
+        Assert.That(solver.Check(), Is.EqualTo(Z3Status.Satisfiable));
+        var model = solver.GetModel();
+        Assert.That(model.GetBitVec(bvConst).Value, Is.EqualTo(new BigInteger(42)));
+        Assert.That(bvConst.ToString(), Does.Contain("variableName"));
+    }
+
+    [TestCase(42u)]
+    [TestCase(0u)]
+    [TestCase(255u)]
+    public void ImplicitConversion_FromUintToBvExpr_Works(uint value)
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        BvExpr<Size32> implicitExpr = value;
+
+        Assert.That(solver.Check(), Is.EqualTo(Z3Status.Satisfiable));
+        var model = solver.GetModel();
+        Assert.That(model.GetBitVec(implicitExpr).Value, Is.EqualTo(new BigInteger(value)));
+    }
+
+    [TestCase(42)]
+    [TestCase(0)]
+    [TestCase(-1)]
+    public void ImplicitConversion_FromIntToBvExpr_Works(int value)
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        BvExpr<Size32> implicitExpr = value;
+
+        Assert.That(solver.Check(), Is.EqualTo(Z3Status.Satisfiable));
+        var model = solver.GetModel();
+        Assert.That(model.GetBitVec(implicitExpr).Value, Is.EqualTo(new BigInteger(unchecked((uint)value))));
+    }
+
+    [Test]
+    public void ImplicitConversion_FromUlongToBvExpr_Works()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        BvExpr<Size64> implicitExpr = 9876543210UL;
+
+        Assert.That(solver.Check(), Is.EqualTo(Z3Status.Satisfiable));
+        var model = solver.GetModel();
+        Assert.That(model.GetBitVec(implicitExpr).Value, Is.EqualTo(new BigInteger(9876543210UL)));
+    }
+
+    [Test]
+    public void ImplicitConversion_FromLongToBvExpr_Works()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        BvExpr<Size64> implicitExpr = -42L;
+
+        Assert.That(solver.Check(), Is.EqualTo(Z3Status.Satisfiable));
+        var model = solver.GetModel();
+        Assert.That(model.GetBitVec(implicitExpr).Value, Is.EqualTo(new BigInteger(unchecked((ulong)-42L))));
+    }
+
+    [Test]
+    public void ImplicitConversion_FromBigIntegerToBvExpr_Works()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bigValue = BigInteger.Parse("12345678901234567890");
+        BvExpr<Size64> implicitExpr = bigValue;
+
+        Assert.That(solver.Check(), Is.EqualTo(Z3Status.Satisfiable));
+        var model = solver.GetModel();
+        var expected = bigValue & ((BigInteger.One << 64) - 1);
+        Assert.That(model.GetBitVec(implicitExpr).Value, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void ImplicitConversion_FromBvValueToBvExpr_Works()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvValue = new Bv<Size32>(42u);
+        BvExpr<Size32> implicitExpr = bvValue;
+
+        Assert.That(solver.Check(), Is.EqualTo(Z3Status.Satisfiable));
+        var model = solver.GetModel();
+        Assert.That(model.GetBitVec(implicitExpr).Value, Is.EqualTo(new BigInteger(42)));
+    }
+
+    [Test]
+    public void CreateMultipleBitVecConstants_HaveIndependentValues()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bv1 = context.BitVecConst<Size32>("var1");
+        var bv2 = context.BitVecConst<Size32>("var2");
+        var bv3 = context.BitVecConst<Size32>("var3");
+
+        solver.Assert(bv1 == 10u);
+        solver.Assert(bv2 == 20u);
+        solver.Assert(bv3 == 30u);
+
+        Assert.That(solver.Check(), Is.EqualTo(Z3Status.Satisfiable));
+        var model = solver.GetModel();
+        Assert.Multiple(() =>
+        {
+            Assert.That(model.GetBitVec(bv1).Value, Is.EqualTo(new BigInteger(10)));
+            Assert.That(model.GetBitVec(bv2).Value, Is.EqualTo(new BigInteger(20)));
+            Assert.That(model.GetBitVec(bv3).Value, Is.EqualTo(new BigInteger(30)));
+        });
+    }
+
+    [Test]
+    public void BitVecConstWithSameName_ReturnsSameHandle()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+
+        var bv1 = context.BitVecConst<Size32>("sameName");
+        var bv2 = context.BitVecConst<Size32>("sameName");
+
+        Assert.That(bv1.Handle, Is.EqualTo(bv2.Handle));
+    }
+
+    [TestCase(TypeArgs = [typeof(Size8)])]
+    [TestCase(TypeArgs = [typeof(Size16)])]
+    [TestCase(TypeArgs = [typeof(Size32)])]
+    [TestCase(TypeArgs = [typeof(Size64)])]
+    public void BvExpr_Sort_ReturnsBvSortKind<TSize>()
+        where TSize : ISize
+    {
+        using var context = new Z3Context();
+
+        var sortHandle = context.GetSortForType<BvExpr<TSize>>();
+        var sortKind = context.Library.Z3GetSortKind(context.Handle, sortHandle);
+
+        Assert.That(sortKind, Is.EqualTo(Z3SortKind.Bv));
+    }
+
+    [TestCase(TypeArgs = [typeof(Size8)], ExpectedResult = 8u)]
+    [TestCase(TypeArgs = [typeof(Size16)], ExpectedResult = 16u)]
+    [TestCase(TypeArgs = [typeof(Size32)], ExpectedResult = 32u)]
+    [TestCase(TypeArgs = [typeof(Size64)], ExpectedResult = 64u)]
+    public uint BvExpr_Size_ReturnsCorrectBitWidth<TSize>()
+        where TSize : ISize
+    {
+        return BvExpr<TSize>.Size;
+    }
+}
