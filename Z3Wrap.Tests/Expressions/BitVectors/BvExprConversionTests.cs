15e434ed0b0d599694171f372bbb9a8bfb76f2b9
spaceorc <spaceorc@yandex.ru>
2025-10-01 00:21:45 +0100

    feat: update bit-vector methods to remove unnecessary parameters and add new tests for resizing and extraction

--- Z3Wrap.Tests/Expressions/BitVectors/BvExprConversionTests.cs
+++ Z3Wrap.Tests/Expressions/BitVectors/BvExprConversionTests.cs
@@ -62,7 +62,7 @@ public class BvExprConversionTests
 
         // This would be negative if interpreted as signed
         var bvValue = context.BitVec<Size32>(0xFFFFFF00u);
-        var intValue = bvValue.ToInt(false);
+        var intValue = bvValue.ToInt();
 
         solver.Check();
         var model = solver.GetModel();
@@ -105,8 +105,8 @@ public class BvExprConversionTests
         using var solver = context.CreateSolver();
 
         var bvValue = context.BitVec<TInputSize>(42u);
-        var resizedValue = bvValue.Resize<TOutputSize>(false);
-        var resizedValueViaContext = context.Resize<TInputSize, TOutputSize>(bvValue, false);
+        var resizedValue = bvValue.Resize<TOutputSize>();
+        var resizedValueViaContext = context.Resize<TInputSize, TOutputSize>(bvValue);
 
         solver.Check();
         var model = solver.GetModel();
@@ -145,7 +145,7 @@ public class BvExprConversionTests
 
         // -42 in 8-bit two's complement (0xD6 = 214)
         var bvValue = context.BitVec<Size8>((uint)unchecked((byte)-42));
-        var resizedValue = bvValue.Resize<Size32>(false);
+        var resizedValue = bvValue.Resize<Size32>();
 
         solver.Check();
         var model = solver.GetModel();
@@ -177,6 +177,31 @@ public class BvExprConversionTests
         Assert.That(model.GetBitVec(truncatedValue).Value, Is.EqualTo(expected));
     }
 
+    [TestCase(TypeArgs = [typeof(Size8)])]
+    [TestCase(TypeArgs = [typeof(Size16)])]
+    [TestCase(TypeArgs = [typeof(Size32)])]
+    [TestCase(TypeArgs = [typeof(Size64)])]
+    public void Resize_SameSize_PreservesValue<TSize>()
+        where TSize : ISize
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvValue = context.BitVec<TSize>(42u);
+        var resizedValue = bvValue.Resize<TSize>();
+        var resizedValueViaContext = context.Resize<TSize, TSize>(bvValue);
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model.GetBitVec(resizedValue).Value, Is.EqualTo(new BigInteger(42)));
+            Assert.That(model.GetBitVec(resizedValueViaContext).Value, Is.EqualTo(new BigInteger(42)));
+        });
+    }
+
     [TestCase(0u)]
     [TestCase(4u)]
     public void Extract_ExtractsCorrectBits(uint startBit)
@@ -199,6 +224,46 @@ public class BvExprConversionTests
             Assert.That(model.GetBitVec(extractedValue).Value, Is.EqualTo(new BigInteger(expectedValue)));
             Assert.That(model.GetBitVec(extractedValueViaContext).Value, Is.EqualTo(new BigInteger(expectedValue)));
         });
+    }
+
+    [Test]
+    public void Extract_ExceedingBounds_ThrowsArgumentException()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+
+        var bvValue = context.BitVec<Size16>(0xABCDu);
+
+        // Trying to extract 8 bits starting at bit 9 would exceed 16-bit bounds
+        Assert.Multiple(() =>
+        {
+            var ex1 = Assert.Throws<ArgumentException>(() => bvValue.Extract<Size8>(9u));
+            Assert.That(ex1!.Message, Does.Contain("Extraction would exceed input bounds"));
+            Assert.That(ex1.Message, Does.Contain("startBit(9)"));
+            Assert.That(ex1.Message, Does.Contain("outputSize(8)"));
+            Assert.That(ex1.Message, Does.Contain("inputSize(16)"));
+
+            var ex2 = Assert.Throws<ArgumentException>(() => context.Extract<Size16, Size8>(bvValue, 9u));
+            Assert.That(ex2!.Message, Does.Contain("Extraction would exceed input bounds"));
+        });
+    }
+
+    [Test]
+    public void Extract_ExactBoundary_Works()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+        using var solver = context.CreateSolver();
+
+        var bvValue = context.BitVec<Size16>(0xABCDu);
+        // Extract 8 bits starting at bit 8 (exactly at boundary)
+        var extractedValue = bvValue.Extract<Size8>(8u);
+
+        solver.Check();
+        var model = solver.GetModel();
+
+        // Should extract bits [15:8] = 0xAB
+        Assert.That(model.GetBitVec(extractedValue).Value, Is.EqualTo(new BigInteger(0xABu)));
     }
 
     [Test]
@@ -238,6 +303,31 @@ public class BvExprConversionTests
 
         // Should be 0x12121212
         Assert.That(model.GetBitVec(repeatedValue).Value, Is.EqualTo(new BigInteger(0x12121212u)));
+    }
+
+    [Test]
+    public void Repeat_TargetSizeNotMultiple_ThrowsArgumentException()
+    {
+        using var context = new Z3Context();
+        using var scope = context.SetUp();
+
+        var bvValue = context.BitVec<Size16>(0xABCDu);
+
+        // Trying to repeat 16-bit value into 20-bit value (20 % 16 != 0)
+        Assert.Multiple(() =>
+        {
+            var ex1 = Assert.Throws<ArgumentException>(() => bvValue.Repeat<Size20>());
+            Assert.That(ex1!.Message, Does.Contain("Target size 20 must be a multiple of source size 16"));
+
+            var ex2 = Assert.Throws<ArgumentException>(() => context.Repeat<Size16, Size20>(bvValue));
+            Assert.That(ex2!.Message, Does.Contain("Target size 20 must be a multiple of source size 16"));
+        });
+    }
+
+    // Custom size for testing non-multiple case
+    private readonly struct Size20 : ISize
+    {
+        public static uint Size => 20;
     }
 
     [Test]
